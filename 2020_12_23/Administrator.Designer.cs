fff16e39667a89966d2f58dc56777a17b15f0711
Agoni <877658620@qq.com>
2020-12-23 22:29:29 +0800

    C#大作业 -- 课程管理系统（管理者）

---
+++ 2020_12_23/Administrator.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace V_1
+{
+    partial class Administrator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.用户管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.添加用户ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.删除用户ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.修改用户ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查看用户ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.课程管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.添加课程ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.修改课程ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.删除课程ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查看课程信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.班级管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.添加班级ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.修改班级ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.删除班级ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查看班级信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出登录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.用户管理ToolStripMenuItem,
+            this.课程管理ToolStripMenuItem,
+            this.班级管理ToolStripMenuItem,
+            this.退出登录ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(463, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 用户管理ToolStripMenuItem
+            // 
+            this.用户管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.添加用户ToolStripMenuItem,
+            this.删除用户ToolStripMenuItem,
+            this.修改用户ToolStripMenuItem,
+            this.查看用户ToolStripMenuItem});
+            this.用户管理ToolStripMenuItem.Name = "用户管理ToolStripMenuItem";
+            this.用户管理ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.用户管理ToolStripMenuItem.Text = "用户管理";
+            this.用户管理ToolStripMenuItem.Click += new System.EventHandler(this.用户管理ToolStripMenuItem_Click);
+            // 
+            // 添加用户ToolStripMenuItem
+            // 
+            this.添加用户ToolStripMenuItem.Name = "添加用户ToolStripMenuItem";
+            this.添加用户ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.添加用户ToolStripMenuItem.Text = "添加用户";
+            // 
+            // 删除用户ToolStripMenuItem
+            // 
+            this.删除用户ToolStripMenuItem.Name = "删除用户ToolStripMenuItem";
+            this.删除用户ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.删除用户ToolStripMenuItem.Text = "删除用户";
+            // 
+            // 修改用户ToolStripMenuItem
+            // 
+            this.修改用户ToolStripMenuItem.Name = "修改用户ToolStripMenuItem";
+            this.修改用户ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.修改用户ToolStripMenuItem.Text = "修改用户";
+            // 
+            // 查看用户ToolStripMenuItem
+            // 
+            this.查看用户ToolStripMenuItem.Name = "查看用户ToolStripMenuItem";
+            this.查看用户ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.查看用户ToolStripMenuItem.Text = "查看用户";
+            // 
+            // 课程管理ToolStripMenuItem
+            // 
+            this.课程管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.添加课程ToolStripMenuItem,
+            this.修改课程ToolStripMenuItem,
+            this.删除课程ToolStripMenuItem,
+            this.查看课程信息ToolStripMenuItem});
+            this.课程管理ToolStripMenuItem.Name = "课程管理ToolStripMenuItem";
+            this.课程管理ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.课程管理ToolStripMenuItem.Text = "课程管理";
+            this.课程管理ToolStripMenuItem.Click += new System.EventHandler(this.课程管理ToolStripMenuItem_Click);
+            // 
+            // 添加课程ToolStripMenuItem
+            // 
+            this.添加课程ToolStripMenuItem.Name = "添加课程ToolStripMenuItem";
+            this.添加课程ToolStripMenuItem.Size = new System.Drawing.Size(174, 26);
+            this.添加课程ToolStripMenuItem.Text = "添加课程";
+            // 
+            // 修改课程ToolStripMenuItem
+            // 
+            this.修改课程ToolStripMenuItem.Name = "修改课程ToolStripMenuItem";
+            this.修改课程ToolStripMenuItem.Size = new System.Drawing.Size(174, 26);
+            this.修改课程ToolStripMenuItem.Text = "修改课程";
+            // 
+            // 删除课程ToolStripMenuItem
+            // 
+            this.删除课程ToolStripMenuItem.Name = "删除课程ToolStripMenuItem";
+            this.删除课程ToolStripMenuItem.Size = new System.Drawing.Size(174, 26);
+            this.删除课程ToolStripMenuItem.Text = "删除课程";
+            // 
+            // 查看课程信息ToolStripMenuItem
+            // 
+            this.查看课程信息ToolStripMenuItem.Name = "查看课程信息ToolStripMenuItem";
+            this.查看课程信息ToolStripMenuItem.Size = new System.Drawing.Size(174, 26);
+            this.查看课程信息ToolStripMenuItem.Text = "查看课程信息";
+            // 
+            // 班级管理ToolStripMenuItem
+            // 
+            this.班级管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.添加班级ToolStripMenuItem,
+            this.修改班级ToolStripMenuItem,
+            this.删除班级ToolStripMenuItem,
+            this.查看班级信息ToolStripMenuItem});
+            this.班级管理ToolStripMenuItem.Name = "班级管理ToolStripMenuItem";
+            this.班级管理ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.班级管理ToolStripMenuItem.Text = "班级管理";
+            this.班级管理ToolStripMenuItem.Click += new System.EventHandler(this.班级管理ToolStripMenuItem_Click);
+            // 
+            // 添加班级ToolStripMenuItem
+            // 
+            this.添加班级ToolStripMenuItem.Name = "添加班级ToolStripMenuItem";
+            this.添加班级ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.添加班级ToolStripMenuItem.Text = "添加班级";
+            // 
+            // 修改班级ToolStripMenuItem
+            // 
+            this.修改班级ToolStripMenuItem.Name = "修改班级ToolStripMenuItem";
+            this.修改班级ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.修改班级ToolStripMenuItem.Text = "修改班级";
+            // 
+            // 删除班级ToolStripMenuItem
+            // 
+            this.删除班级ToolStripMenuItem.Name = "删除班级ToolStripMenuItem";
+            this.删除班级ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.删除班级ToolStripMenuItem.Text = "删除班级";
+            // 
+            // 查看班级信息ToolStripMenuItem
+            // 
+            this.查看班级信息ToolStripMenuItem.Name = "查看班级信息ToolStripMenuItem";
+            this.查看班级信息ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.查看班级信息ToolStripMenuItem.Text = "查看班级信息";
+            // 
+            // 退出登录ToolStripMenuItem
+            // 
+            this.退出登录ToolStripMenuItem.Name = "退出登录ToolStripMenuItem";
+            this.退出登录ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.退出登录ToolStripMenuItem.Text = "退出登录";
+            this.退出登录ToolStripMenuItem.Click += new System.EventHandler(this.退出登录ToolStripMenuItem_Click);
+            // 
+            // Administrator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 283);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Administrator";
+            this.Text = "Administrator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 用户管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 添加用户ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 删除用户ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 修改用户ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查看用户ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 课程管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 添加课程ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 修改课程ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 删除课程ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 班级管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 添加班级ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 修改班级ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 删除班级ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出登录ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查看课程信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查看班级信息ToolStripMenuItem;
+    }
+}
---
+++ 2020_12_23/Administrator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace V_1
+{
+    public partial class Administrator : Form
+    {
+        public Administrator()
+        {
+            InitializeComponent();
+        }
+
+        private void 退出登录ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void 用户管理ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            control_user f2 = new control_user();
+            f2.Show();
+        }
+
+        private void 课程管理ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            control_course f2 = new control_course();
+            f2.Show();
+        }
+
+        private void 班级管理ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            control_user f2 = new control_user();
+            f2.Show();
+        }
+    }
+}
