f0bc854bab7bb85f1c8f6b7f794fe58697b58beb
Agoni <877658620@qq.com>
2020-12-25 23:11:17 +0800

    C#大作业 -- 课程管理系统（用户控制）

---
+++ 2020_12_25/control_user.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace V_1
+{
+    partial class control_user
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSeach = new System.Windows.Forms.Button();
+            this.RbtnFamale = new System.Windows.Forms.RadioButton();
+            this.RbtnMale = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAllStu = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSeach);
+            this.groupBox1.Controls.Add(this.RbtnFamale);
+            this.groupBox1.Controls.Add(this.RbtnMale);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(593, 121);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "学生信息";
+            // 
+            // btnSeach
+            // 
+            this.btnSeach.Location = new System.Drawing.Point(413, 87);
+            this.btnSeach.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSeach.Name = "btnSeach";
+            this.btnSeach.Size = new System.Drawing.Size(67, 23);
+            this.btnSeach.TabIndex = 16;
+            this.btnSeach.Text = "查看";
+            this.btnSeach.UseVisualStyleBackColor = true;
+            this.btnSeach.Click += new System.EventHandler(this.btnSeach_Click);
+            // 
+            // RbtnFamale
+            // 
+            this.RbtnFamale.AutoSize = true;
+            this.RbtnFamale.Location = new System.Drawing.Point(536, 21);
+            this.RbtnFamale.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.RbtnFamale.Name = "RbtnFamale";
+            this.RbtnFamale.Size = new System.Drawing.Size(43, 19);
+            this.RbtnFamale.TabIndex = 15;
+            this.RbtnFamale.TabStop = true;
+            this.RbtnFamale.Text = "女";
+            this.RbtnFamale.UseVisualStyleBackColor = true;
+            // 
+            // RbtnMale
+            // 
+            this.RbtnMale.AutoSize = true;
+            this.RbtnMale.Location = new System.Drawing.Point(486, 21);
+            this.RbtnMale.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.RbtnMale.Name = "RbtnMale";
+            this.RbtnMale.Size = new System.Drawing.Size(43, 19);
+            this.RbtnMale.TabIndex = 14;
+            this.RbtnMale.TabStop = true;
+            this.RbtnMale.Text = "男";
+            this.RbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(504, 87);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "返回";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(303, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "删除";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "修改";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "添加";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(303, 49);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(134, 25);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(249, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "班级";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(90, 49);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(134, 25);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "入学时间";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(443, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "性别";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(303, 18);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 25);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "姓名";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "学号";
+            // 
+            // txtAllStu
+            // 
+            this.txtAllStu.Location = new System.Drawing.Point(12, 149);
+            this.txtAllStu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtAllStu.Multiline = true;
+            this.txtAllStu.Name = "txtAllStu";
+            this.txtAllStu.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAllStu.Size = new System.Drawing.Size(593, 151);
+            this.txtAllStu.TabIndex = 2;
+            this.txtAllStu.TextChanged += new System.EventHandler(this.txtAllStu_TextChanged);
+            // 
+            // control_user
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 311);
+            this.Controls.Add(this.txtAllStu);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "control_user";
+            this.Text = "control_user";
+            this.Load += new System.EventHandler(this.control_user_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton RbtnFamale;
+        private System.Windows.Forms.RadioButton RbtnMale;
+        private System.Windows.Forms.TextBox txtAllStu;
+        private System.Windows.Forms.Button btnSeach;
+    }
+}
---
+++ 2020_12_25/control_user.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace V_1
+{
+    public partial class control_user : Form
+    {
+        string strcon = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CourseManagementSystem;Integrated Security=True";
+
+        public control_user()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void control_user_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(strcon);
+            try
+            {
+                string Student_id = textBox1.Text.Trim();
+                string Student_name = textBox2.Text.Trim();
+                string Student_sex;
+                if (RbtnMale.Checked)
+                {
+                    Student_sex = "male";
+                }
+                else
+                {
+                    Student_sex = "fmale";
+                }
+                string Student_time = dateTimePicker1.Value.ToShortDateString().Split(' ')[0];
+                string Student_class = textBox3.Text.Trim();
+                //string stuCredit = txtCredit.Text.Trim();
+                SqlCommand command = new SqlCommand();
+                //command.CommandText = "INSERT INTO Student(Student_id，Student_name，Student_sex，Student_time，Student_class)VALUES('" + Student_id + "', '" + Student_name + "', '" + Student_sex + "', '" + Student_time + "', '" + Student_class + "')";
+                command.CommandText = "INSERT INTO Student(Student_id，Student_name，Student_sex，Student_time，Student_class)VALUES(" + Student_id + ", " + Student_name + ", " + Student_sex + ", " + Student_time + ", " + Student_class + ")";
+                command.CommandType = CommandType.Text;
+                command.Connection = sqlcon;
+                sqlcon.Open();
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("插入成功！", "消息", MessageBoxButtons.OK);
+                    btnSeach_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+        }
+
+        private void btnSeach_Click(object sender, EventArgs e)
+        {
+            txtAllStu.Clear();
+            using (SqlConnection sqlcon = new SqlConnection(strcon))
+            {
+                sqlcon.Open();
+                string sql = "SELECT * FROM Student";
+                SqlCommand command = new SqlCommand(sql, sqlcon);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        txtAllStu.Text += reader[0].ToString() + "    " + reader[1].ToString()
+    + "    " + reader[2].ToString() + "    " + reader[3].ToString() + "    " + reader[4].ToString() + "\r\n";
+                    }
+                }
+                reader.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Administrator f2 = new Administrator();
+            f2.Show();
+        }
+
+        private void txtAllStu_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
