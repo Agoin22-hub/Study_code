47022fb5563a10060cba359367df50d346118386
Agoni <877658620@qq.com>
2020-12-26 20:27:57 +0800

    C#大作业 -- 课程管理系统（课程）

---
+++ 2020_12_26/Course.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace V_1
+{
+    partial class Course
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAllStu = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 194);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(608, 102);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "选课";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(497, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "返回";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(416, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "添加选课";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(228, 21);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 25);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "课程名";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(64, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 56);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(593, 40);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "课程号";
+            // 
+            // txtAllStu
+            // 
+            this.txtAllStu.Location = new System.Drawing.Point(12, 33);
+            this.txtAllStu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtAllStu.Multiline = true;
+            this.txtAllStu.Name = "txtAllStu";
+            this.txtAllStu.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAllStu.Size = new System.Drawing.Size(608, 156);
+            this.txtAllStu.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "课程列表";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Course
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 311);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtAllStu);
+            this.Name = "Course";
+            this.Text = "Course";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAllStu;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ 2020_12_26/Course.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace V_1
+{
+    public partial class Course : Form
+    {
+        string strcon = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CourseManagementSystem;Integrated Security=True";
+
+        public Course()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            using (SqlConnection sqlcon = new SqlConnection(strcon))
+            {
+                sqlcon.Open();
+
+                string sql = "SELECT * FROM Course where Course_id='" + textBox2.Text + "'And Course_name='" + textBox3.Text + "'";
+                SqlCommand command = new SqlCommand(sql, sqlcon);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        txtAllStu.Text += reader[0].ToString() + "    " + reader[1].ToString()
+    + "    " + reader[2].ToString() + "    " + reader[3].ToString() + "    " + reader[4].ToString() + "\r\n";
+                    }
+                }
+                reader.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(strcon);
+            try
+            {
+                int i = 1;
+                string Student_id = Login.ID;
+                string Course_id = textBox2.Text;
+
+                SqlCommand command = new SqlCommand();
+                //command.CommandText = "INSERT INTO Student(Student_id，Student_name，Student_sex，Student_time，Student_class)VALUES('" + Student_id + "', '" + Student_name + "', '" + Student_sex + "', '" + Student_time + "', '" + Student_class + "')";
+                command.CommandText = "INSERT INTO Student_Course(ID,Student_id，Course_id)VALUES('" + i++ + "','" + Student_id + "','" + Course_id + "')";
+                command.CommandType = CommandType.Text;
+                command.Connection = sqlcon;
+                sqlcon.Open();
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("插入成功！", "消息", MessageBoxButtons.OK);
+                    //btnSeach_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Student f2 = new Student();
+            f2.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtAllStu.Clear();
+            using (SqlConnection sqlcon = new SqlConnection(strcon))
+            {
+                sqlcon.Open();
+                string sql = "SELECT * FROM Student";
+                SqlCommand command = new SqlCommand(sql, sqlcon);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        txtAllStu.Text += reader[0].ToString() + "    " + reader[1].ToString()
+    + "    " + reader[2].ToString() + "    " + reader[3].ToString() + "    " + reader[4].ToString() + "\r\n";
+                    }
+                }
+                reader.Close();
+            }
+        }
+    }
+}
