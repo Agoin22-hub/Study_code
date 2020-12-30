71363568b2fd0b484a8d0862dd65f5a4df6befd0
Agoni <877658620@qq.com>
2020-12-30 20:57:05 +0800

    C#大作业 -- 课程管理系统（学生）

---
+++ 2020_12_30/Student.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace V_1
+{
+    partial class Student
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.个人信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.选课列表ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.班级信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出登录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.个人信息ToolStripMenuItem,
+            this.选课列表ToolStripMenuItem,
+            this.班级信息ToolStripMenuItem,
+            this.退出登录ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(415, 30);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 个人信息ToolStripMenuItem
+            // 
+            this.个人信息ToolStripMenuItem.Name = "个人信息ToolStripMenuItem";
+            this.个人信息ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.个人信息ToolStripMenuItem.Text = "个人信息";
+            this.个人信息ToolStripMenuItem.Click += new System.EventHandler(this.个人信息ToolStripMenuItem_Click);
+            // 
+            // 选课列表ToolStripMenuItem
+            // 
+            this.选课列表ToolStripMenuItem.Name = "选课列表ToolStripMenuItem";
+            this.选课列表ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.选课列表ToolStripMenuItem.Text = "选课列表";
+            this.选课列表ToolStripMenuItem.Click += new System.EventHandler(this.选课列表ToolStripMenuItem_Click);
+            // 
+            // 班级信息ToolStripMenuItem
+            // 
+            this.班级信息ToolStripMenuItem.Name = "班级信息ToolStripMenuItem";
+            this.班级信息ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.班级信息ToolStripMenuItem.Text = "查询结果";
+            this.班级信息ToolStripMenuItem.Click += new System.EventHandler(this.班级信息ToolStripMenuItem_Click);
+            // 
+            // 退出登录ToolStripMenuItem
+            // 
+            this.退出登录ToolStripMenuItem.Name = "退出登录ToolStripMenuItem";
+            this.退出登录ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.退出登录ToolStripMenuItem.Text = "退出登录";
+            this.退出登录ToolStripMenuItem.Click += new System.EventHandler(this.退出登录ToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 47);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(372, 58);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Student
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 171);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Student";
+            this.Text = "Student";
+            this.Load += new System.EventHandler(this.Student_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 个人信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 选课列表ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 班级信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出登录ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ 2020_12_30/Student.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace V_1
+{
+    public partial class Student : Form
+    {
+        string strcon = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CourseManagementSystem;Integrated Security=True";
+
+        public Student()
+        {
+            InitializeComponent();
+        }
+
+        private void Student_Load(object sender, EventArgs e)
+        {
+            string Student_id = Login.ID;
+        }
+
+        private void 退出登录ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void 个人信息ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            using (SqlConnection sqlcon = new SqlConnection(strcon))
+            {
+                sqlcon.Open();
+                string sql  = "select *from Student where Student_id='" +  Login.ID+"'" ;
+                SqlCommand command = new SqlCommand(sql, sqlcon);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        textBox1.Text +=  reader[0].ToString() + "    " + reader[1].ToString()
+    + "    " + reader[2].ToString() + "    " + reader[3].ToString() + "    " + reader[4].ToString() + "\r\n";
+
+                    }
+                }
+                reader.Close();
+            }            
+        }
+
+        private void 班级信息ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            using (SqlConnection sqlcon = new SqlConnection(strcon))
+            {
+                sqlcon.Open();
+                string sql = "select *from Student_Course where Student_id='" + Login.ID + "'";
+                SqlCommand command = new SqlCommand(sql, sqlcon);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        textBox1.Text += "选课号： " + reader[0].ToString() + "    学号： "+ reader[1].ToString() + "    课程号"
+    + reader[2].ToString()  + "\r\n";
+
+                    }
+                }
+                reader.Close();
+            }
+        }
+
+        private void 选课列表ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Course f2 = new Course();
+            f2.Show();
+        }
+    }
+}
+
