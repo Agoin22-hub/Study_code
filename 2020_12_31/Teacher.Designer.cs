05dc0d3cad09ed054e07a2dffdf98a7a6672f9b1
Agoni <877658620@qq.com>
2020-12-31 22:53:54 +0800

    C#大作业 -- 课程管理系统（老师）

---
+++ 2020_12_31/Teacher.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace V_1
+{
+    partial class Teacher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.个人信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.课程信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.班级信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出登录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtAllTea = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.个人信息ToolStripMenuItem,
+            this.课程信息ToolStripMenuItem,
+            this.班级信息ToolStripMenuItem,
+            this.退出登录ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(535, 30);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 个人信息ToolStripMenuItem
+            // 
+            this.个人信息ToolStripMenuItem.Name = "个人信息ToolStripMenuItem";
+            this.个人信息ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.个人信息ToolStripMenuItem.Text = "个人信息";
+            this.个人信息ToolStripMenuItem.Click += new System.EventHandler(this.个人信息ToolStripMenuItem_Click);
+            // 
+            // 课程信息ToolStripMenuItem
+            // 
+            this.课程信息ToolStripMenuItem.Name = "课程信息ToolStripMenuItem";
+            this.课程信息ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.课程信息ToolStripMenuItem.Text = "课程信息";
+            this.课程信息ToolStripMenuItem.Click += new System.EventHandler(this.课程信息ToolStripMenuItem_Click);
+            // 
+            // 班级信息ToolStripMenuItem
+            // 
+            this.班级信息ToolStripMenuItem.Name = "班级信息ToolStripMenuItem";
+            this.班级信息ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.班级信息ToolStripMenuItem.Text = "班级信息";
+            this.班级信息ToolStripMenuItem.Click += new System.EventHandler(this.班级信息ToolStripMenuItem_Click);
+            // 
+            // 退出登录ToolStripMenuItem
+            // 
+            this.退出登录ToolStripMenuItem.Name = "退出登录ToolStripMenuItem";
+            this.退出登录ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.退出登录ToolStripMenuItem.Text = "退出登录";
+            this.退出登录ToolStripMenuItem.Click += new System.EventHandler(this.退出登录ToolStripMenuItem_Click);
+            // 
+            // txtAllTea
+            // 
+            this.txtAllTea.Location = new System.Drawing.Point(12, 49);
+            this.txtAllTea.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtAllTea.Multiline = true;
+            this.txtAllTea.Name = "txtAllTea";
+            this.txtAllTea.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAllTea.Size = new System.Drawing.Size(511, 169);
+            this.txtAllTea.TabIndex = 3;
+            // 
+            // Teacher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 246);
+            this.Controls.Add(this.txtAllTea);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Teacher";
+            this.Text = "Teacher";
+            this.Load += new System.EventHandler(this.Teacher_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 个人信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 课程信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 班级信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出登录ToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtAllTea;
+    }
+}
---
+++ 2020_12_31/Teacher.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace V_1
+{
+    public partial class Teacher : Form
+    {
+        string strcon = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CourseManagementSystem;Integrated Security=True";
+
+        public Teacher()
+        {
+            InitializeComponent();
+        }
+
+        private void 退出登录ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        string n;
+        private void 个人信息ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtAllTea.Clear();
+            using (SqlConnection sqlcon = new SqlConnection(strcon))
+            {
+                sqlcon.Open();
+                string sql = "select *from Teacher where Teacher_id='" + Login.ID + "'";
+                SqlCommand command = new SqlCommand(sql, sqlcon);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        txtAllTea.Text += "教工号： " + reader[0].ToString() + "  姓名：" + reader[1].ToString()
+    + "  电话：" + reader[2].ToString() + "  课程号：" + reader[3].ToString() + "\r\n";
+
+                    }
+                   // string n = reader[2].ToString();
+                }
+                reader.Close();
+            }
+        }
+
+        private void 班级信息ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtAllTea.Clear();
+            using (SqlConnection sqlcon = new SqlConnection(strcon))
+            {
+                sqlcon.Open();
+                string sql = "select *from Student ";
+                SqlCommand command = new SqlCommand(sql, sqlcon);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        txtAllTea.Text += "班级： " + reader[4].ToString() +  "\r\n";
+                    }
+                }
+                reader.Close();
+            }
+        }
+
+        private void 课程信息ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtAllTea.Clear();
+            using (SqlConnection sqlcon = new SqlConnection(strcon))
+            {
+                sqlcon.Open();
+                string sql = "select *from Teacher where Course_id='" + n + "'";
+                SqlCommand command = new SqlCommand(sql, sqlcon);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        txtAllTea.Text += "教工号： " + reader[0].ToString() + "  姓名：" + reader[1].ToString()
+    + "  电话：" + reader[2].ToString() + "  课程号：" + reader[3].ToString() + "\r\n";
+
+                    }
+                }
+                reader.Close();
+            }
+        }
+
+        private void Teacher_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
